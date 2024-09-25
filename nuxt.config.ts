8d9bfd9aea0de1ca295f7876c7474a2bdb4be389
Hassan Barbour <barbour.hassan@gmail.com>
2024-09-25 18:19:26 +0300

    thirteen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  // ssr: false,
-  // typescript: {
-  //   shim: false,
-  // },
+  ssr: false,
+  typescript: {
+    shim: false,
+  },
   app: {
     head: {
         meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
@@ -19,8 +19,8 @@ export default defineNuxtConfig({
     transpile: ["vuetify"],
   },
   nitro: {
-    // serveStatic: true,
-    // preset: "cloudflare-pages"
+    serveStatic: true,
+    preset: "cloudflare-pages"
   },
   sourcemap: { server: false, client: false },
   devServerHandlers: [],
