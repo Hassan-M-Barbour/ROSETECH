60107535fdffdb19245f81dbf1eec162c24b465d
Hassan Barbour <barbour.hassan@gmail.com>
2024-09-25 18:43:36 +0300

    twenty

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import { useDisplay } from "vuetify";
+//import { useDisplay } from "vuetify";
 useHead({
     title: "ROSETECH  Dashboard",
 });
 const drawer = ref(undefined || true);
 //const innerW = window.innerWidth;
-const { mdAndUp, mdAndDown } = useDisplay();
+//const { mdAndUp, mdAndDown } = useDisplay();
 onMounted(() => {
     // if (innerW < 950) {
     //     drawer.value = !drawer.value;
