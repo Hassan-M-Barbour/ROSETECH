2b7b2de9ee9e52a4b1fe4c2930457b4391233c43
Hassan-M-Barbour <162156167+Hassan-M-Barbour@users.noreply.github.com>
2024-09-25 12:58:45 +0300

    Update contacts.put.ts

--- server/api/contacts.put.ts
+++ server/api/contacts.put.ts
@@ -1,1 +1,25 @@
 
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+//   const body = await readBody(event)
+//   let updatedUser = null
+
+//   if (body.id && (body.name && body.email)) {
+//     updatedUser = await prisma.contact.update({
+//       where: { id: body.id },
+//       data: {
+//         name: body.name,
+//         email: body.email,
+//         subject: body.subject,
+//         phone: body.phone,
+//         message: body.message,
+//         emp: body.emp
+   
+//       },
+//     })
+//   }
+
+//   return { user: updatedUser }
+})
