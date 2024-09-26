fff031316bf98d590b018066c68119f95a02f355
Hassan Barbour <barbour.hassan@gmail.com>
2024-09-26 12:55:41 +0300

    thirty seven

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,7 @@ export default defineNuxtConfig({
   // devServerHandlers: [],
 
  // modules: ["@sidebase/nuxt-auth", "@nuxtjs/tailwindcss" , 'nuxt-resend'],
-  modules: [ "@nuxtjs/tailwindcss" , 'nuxt-resend'],
+  modules: ["@sidebase/nuxt-auth", "@nuxtjs/tailwindcss" , 'nuxt-resend'],
+  
   
 })
