e04e405b74cfdfeddf3407854e3639b2be21d7e9
Hassan Barbour <barbour.hassan@gmail.com>
2024-09-27 14:57:16 +0300

    forty two

--- server/api/addContact.ts
+++ server/api/addContact.ts
@@ -5,7 +5,7 @@ const requestBody = await readBody(event);
 
 const db = useDatabase("myDatabase");
 
-const result = await db.sql`INSERT INTO Contact (name,email, subject , phone , message , emp) VALUES (${requestBody.name},${requestBody.email},${requestBody.subject},${requestBody.phone},${requestBody.message},${requestBody.emp})`;
+const result = await db.sql`INSERT INTO Contact (name,email, subject , phone , message , emp) VALUES (${requestBody.name},${requestBody.email},${requestBody.subject},${requestBody.phone},${requestBody.message},${requestBody.employee})`;
    return result;
 
 });
