fbae89b2ff064d6236a6e49c09ad11bac684606c
Hassan Barbour <barbour.hassan@gmail.com>
2024-09-27 18:28:59 +0300

    fift

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -84,10 +84,10 @@ definePageMeta({
 
 
 <template>
-<v-row class="month-table">
+<!-- <v-row class="month-table">
         <v-col cols="12" sm="12" > 
-             <UiChildCard title="Contacts Table">
-                <v-table>
+             <UiChildCard title="Contacts Table"> -->
+                <table class="table-auto">
                     <thead>
                         <tr>
                             <th class="text-left">id</th>
@@ -119,11 +119,11 @@ definePageMeta({
           </td>
                         </tr>
                     </tbody>
-                </v-table>
+                </table>
 <!-- <h1>This ia ROSETECH DASHBOARD !</h1> -->
 
 <!-- <button v-if="LoggenIn" @click="handlesignOut">Sign Out</button> -->
-            </UiChildCard>
+            <!-- </UiChildCard>
         </v-col>
-</v-row>
+</v-row> -->
 </template>
