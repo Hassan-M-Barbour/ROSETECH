30f5ab32223eb244c6a908ca82c94e36388016e2
Hassan Barbour <barbour.hassan@gmail.com>
2025-05-21 16:23:34 +0300

    remove mohamad

--- components/sections/innerpages/Team2.vue
+++ components/sections/innerpages/Team2.vue
@@ -71,16 +71,16 @@
         </div>
 
         <!-- Team block -->
-        <div
+        <!-- <div
           class="team-block-two col-lg-4 col-md-6 wow fadeInUp"
           data-wow-delay="800ms"
         >
           <div class="inner-box">
             <div class="info-box">
               <h4 class="name">
-                <!-- <NuxtLink to="page-team-details"> -->
+               
                 Mohammad H Taresh
-                <!-- </NuxtLink> -->
+                
               </h4>
               <span class="designation"
                 >co-founder<br />
@@ -89,20 +89,14 @@
             </div>
             <div class="image-box">
               <figure class="image">
-                <!-- <NuxtLink to="/page-team-details"> -->
+              
                 <img src="/images/resource/mohamad.jpg" alt="" />
-                <!-- </NuxtLink> -->
+                
               </figure>
-              <!-- <div class="social-links">
-								<NuxtLink to="#"><i class="fab fa-twitter"></i></NuxtLink>
-								<NuxtLink to="#"><i class="fab fa-facebook-f"></i></NuxtLink>
-								<NuxtLink to="#"><i class="fab fa-pinterest-p"></i></NuxtLink>
-								<NuxtLink to="#"><i class="fab fa-instagram"></i></NuxtLink>
-							</div> -->
-              <!-- <span class="share-icon fa fa-plus"></span> -->
+              
             </div>
           </div>
-        </div>
+        </div> -->
 
         <div class="team-block-two col-lg-4 col-md-6 wow fadeInUp">
           <div class="inner-box">
--- components/sections/innerpages/Team2SE.vue
+++ components/sections/innerpages/Team2SE.vue
@@ -71,17 +71,13 @@
         </div>
 
         <!-- Team block -->
-        <div
+        <!-- <div
           class="team-block-two col-lg-4 col-md-6 wow fadeInUp"
           data-wow-delay="800ms"
         >
           <div class="inner-box">
             <div class="info-box">
-              <h4 class="name">
-                <!-- <NuxtLink to="page-team-details"> -->
-                Mohammad H Taresh
-                <!-- </NuxtLink> -->
-              </h4>
+              <h4 class="name">Mohammad H Taresh</h4>
               <span class="designation"
                 >Medgrundare<br />
                 &nbsp;</span
@@ -89,20 +85,11 @@
             </div>
             <div class="image-box">
               <figure class="image">
-                <!-- <NuxtLink to="/page-team-details"> -->
                 <img src="/images/resource/mohamad.jpg" alt="" />
-                <!-- </NuxtLink> -->
               </figure>
-              <!-- <div class="social-links">
-								<NuxtLink to="#"><i class="fab fa-twitter"></i></NuxtLink>
-								<NuxtLink to="#"><i class="fab fa-facebook-f"></i></NuxtLink>
-								<NuxtLink to="#"><i class="fab fa-pinterest-p"></i></NuxtLink>
-								<NuxtLink to="#"><i class="fab fa-instagram"></i></NuxtLink>
-							</div> -->
-              <!-- <span class="share-icon fa fa-plus"></span> -->
             </div>
           </div>
-        </div>
+        </div> -->
 
         <div class="team-block-two col-lg-4 col-md-6 wow fadeInUp">
           <div class="inner-box">
